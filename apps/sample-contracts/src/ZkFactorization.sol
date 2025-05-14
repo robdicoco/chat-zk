9ff7cf0f6123bc56d0df03b27090a275a03e784b
robdicoco <pavusa@gmail.com>
2025-05-14 10:33:39 -0300

    Add zkverifyjs dependency and update package-lock for new packages in sample-contracts

--- apps/sample-contracts/src/ZkFactorization.sol
+++ apps/sample-contracts/src/ZkFactorization.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.13;
 
 contract ZkFactorization {
     /// The hash of the identifier of the proving system used (groth16 in this case)
-    bytes32 public constant PROVING_SYSTEM_ID =
-        keccak256(abi.encodePacked("groth16"));
+    bytes32 public constant PROVING_SYSTEM_ID = keccak256(abi.encodePacked("groth16"));
 
     /// The address of the ZkvAttestationContract
     address public immutable zkvContract;
@@ -21,21 +20,10 @@ contract ZkFactorization {
         vkHash = _vkHash;
     }
 
-    function proveYouCanFactor42(
-        uint256 attestationId,
-        bytes32[] calldata merklePath,
-        uint256 leafCount,
-        uint256 index
-    ) external {
-        require(
-            _verifyProofHasBeenPostedToZkv(
-                attestationId,
-                msg.sender,
-                merklePath,
-                leafCount,
-                index
-            )
-        );
+    function proveYouCanFactor42(uint256 attestationId, bytes32[] calldata merklePath, uint256 leafCount, uint256 index)
+        external
+    {
+        require(_verifyProofHasBeenPostedToZkv(attestationId, msg.sender, merklePath, leafCount, index));
         /// If a valid proof has been posted to zkVerify, then perform app-specific logic (e.g. mint a NFT).
         /// In this simple example, we just record the address of the sender inside a map and emit an event.
         hasSubmittedValidProof[msg.sender] = true;
@@ -49,12 +37,8 @@ contract ZkFactorization {
         uint256 leafCount,
         uint256 index
     ) internal view returns (bool) {
-        bytes memory encodedInput = abi.encodePacked(
-            _changeEndianess(uint256(uint160(inputAddress)))
-        );
-        bytes32 leaf = keccak256(
-            abi.encodePacked(PROVING_SYSTEM_ID, vkHash, keccak256(encodedInput))
-        );
+        bytes memory encodedInput = abi.encodePacked(_changeEndianess(uint256(uint160(inputAddress))));
+        bytes32 leaf = keccak256(abi.encodePacked(PROVING_SYSTEM_ID, vkHash, keccak256(encodedInput)));
 
         (bool callSuccessful, bytes memory validProof) = zkvContract.staticcall(
             abi.encodeWithSignature(
@@ -77,37 +61,17 @@ contract ZkFactorization {
     function _changeEndianess(uint256 input) internal pure returns (uint256 v) {
         v = input;
         // swap bytes
-        v =
-            ((v &
-                0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >>
-                8) |
-            ((v &
-                0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) <<
-                8);
+        v = ((v & 0xFF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00) >> 8)
+            | ((v & 0x00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF00FF) << 8);
         // swap 2-byte long pairs
-        v =
-            ((v &
-                0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >>
-                16) |
-            ((v &
-                0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) <<
-                16);
+        v = ((v & 0xFFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000) >> 16)
+            | ((v & 0x0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF0000FFFF) << 16);
         // swap 4-byte long pairs
-        v =
-            ((v &
-                0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >>
-                32) |
-            ((v &
-                0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) <<
-                32);
+        v = ((v & 0xFFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000) >> 32)
+            | ((v & 0x00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF00000000FFFFFFFF) << 32);
         // swap 8-byte long pairs
-        v =
-            ((v &
-                0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >>
-                64) |
-            ((v &
-                0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) <<
-                64);
+        v = ((v & 0xFFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF0000000000000000) >> 64)
+            | ((v & 0x0000000000000000FFFFFFFFFFFFFFFF0000000000000000FFFFFFFFFFFFFFFF) << 64);
         // swap 16-byte long pairs
         v = (v >> 128) | (v << 128);
     }
